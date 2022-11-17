46abfd1a7ddf140e72f94bf4ba93a66e531b1756
mahdi kohestani <kohestanimahdi@gmail.com>
2022-11-17 21:08:30 +0330

    add application db context test

---
+++ Review.Tests/DataProvider/TestApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Review.Infrastructure.Persistance;
+using System;
+
+namespace Review.Tests.DataProvider
+{
+    internal class TestApplicationDbContext : ApplicationDbContext, IDisposable
+    {
+        private static DbContextOptionsBuilder _optionsBuilder = new();
+        public TestApplicationDbContext(string dbName) : base(_optionsBuilder.UseInMemoryDatabase($"Test_{dbName}").Options)
+        {
+        }
+    }
+}
