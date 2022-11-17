082240a25938f9a1c5f59ed2f6d1b3d504d271c9
mahdi kohestani <kohestanimahdi@gmail.com>
2022-11-17 19:25:45 +0330

    configure the entity type for product review

---
+++ Review.Infrastructure/Persistance/Configs/ProductAggregates/ProductReviewEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Review.Domain.ProductAggregates;
+
+namespace Review.Infrastructure.Persistance.Configs.ProductAggregates
+{
+    internal class ProductReviewEntityTypeConfiguration : IEntityTypeConfiguration<ProductReview>
+    {
+        public void Configure(EntityTypeBuilder<ProductReview> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.Comment).IsRequired();
+            builder.Property(i => i.Title).IsRequired();
+        }
+    }
+}
