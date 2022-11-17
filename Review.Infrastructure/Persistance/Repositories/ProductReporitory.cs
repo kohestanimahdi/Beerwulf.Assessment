61e068463b6852b94bd500a476ce0f037fd5b176
mahdi kohestani <kohestanimahdi@gmail.com>
2022-11-17 19:38:50 +0330

    add product repository

---
+++ Review.Infrastructure/Persistance/Repositories/ProductReporitory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review.Infrastructure.Persistance.Repositories
+{
+    internal class ProductReporitory
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ProductReporitory(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public bool IsAnyExists()
+        => _dbContext.Products.Any();
+    }
+}
