6e04632b7fe27582b9e56315715cae0bbc48a0b6
mahdi kohestani <kohestanimahdi@gmail.com>
2022-11-17 19:17:12 +0330

    adding the base entity

---
+++ Review.Domain/Common/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review.Domain.Common
+{
+    public interface IBaseEntity
+    { }
+
+    public abstract class BaseEntity<T> : IBaseEntity
+    {
+        public BaseEntity()
+        {
+            CreateTime = DateTime.Now;
+        }
+
+        [Key]
+        public T Id { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+
+    public abstract class BaseEntity : BaseEntity<int>
+    { }
+}
