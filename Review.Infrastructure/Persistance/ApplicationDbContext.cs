8f9a5860c60a0a4607dccd7805438d2706f4c1fb
mahdi kohestani <kohestanimahdi@gmail.com>
2022-11-17 19:28:37 +0330

    add the dbcontext to the project

---
+++ Review.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Review.Domain.Common;
+using Review.Domain.ProductAggregates;
+
+namespace Review.Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductReview> ProductReviews { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(IBaseEntity).Assembly);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
