1eb4a135d115f0efa56532e7394c3ef127238d0c
mahdi kohestani <kohestanimahdi@gmail.com>
2022-11-17 19:40:31 +0330

    adding product unit of work

---
+++ Review.Infrastructure/Persistance/UnitOfWorks/IProductUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Review.Infrastructure.Persistance.UnitOfWorks
+{
+    public interface IProductUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Review.Infrastructure/Persistance/UnitOfWorks/ProductUnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Review.Infrastructure.Persistance.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Review.Infrastructure.Persistance.UnitOfWorks
+{
+    public class ProductUnitOfWork : IProductUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ProductReporitory _productReporitory;
+
+        public ProductUnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+            _productReporitory = new(_dbContext);
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+            => _dbContext.SaveChangesAsync(cancellationToken);
+
+        public bool IsAnyProductExists()
+            => _productReporitory.IsAnyExists();
+    }
+}
