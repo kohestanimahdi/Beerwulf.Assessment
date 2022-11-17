4d3761521369d5d8738b1b185df8fa7a6c73a8ed
mahdi kohestani <kohestanimahdi@gmail.com>
2022-11-17 21:10:18 +0330

    adding the test of the product unit of work

---
+++ Review.Tests/UnitOfWorks/TestProductUnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using Review.Domain.ProductAggregates;
+using Review.Infrastructure.Persistance.UnitOfWorks;
+using Review.Tests.DataProvider;
+using System;
+using Xunit;
+
+namespace Review.Tests.UnitOfWorks
+{
+    public class TestProductUnitOfWork : IDisposable
+    {
+        private readonly IProductUnitOfWork _productUnitOfWork;
+        private readonly TestApplicationDbContext _dbContext;
+        public TestProductUnitOfWork()
+        {
+            _dbContext = new TestApplicationDbContext("TestProductUnitOfWork");
+            _productUnitOfWork = new ProductUnitOfWork(_dbContext);
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        [Fact]
+        public void Test_IsAnyProductExists_ReturnsFalse()
+        {
+            var result = _productUnitOfWork.IsAnyProductExists();
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Test_IsAnyProductExists_ReturnsTrue()
+        {
+            var product = new Product
+            {
+                CreateTime = DateTime.Now,
+                Description = "Test",
+                Name = "Test",
+                Price = 1
+            };
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+            var result = _productUnitOfWork.IsAnyProductExists();
+            Assert.True(result);
+
+            _dbContext.Products.RemoveRange(_dbContext.Products);
+            _dbContext.SaveChanges();
+        }
+    }
+}
